89301cd532dcaf987a9abdc957444c1b0eb49af9
Drait <59900506+CodeTheDrait@users.noreply.github.com>
2020-06-26 11:39:01 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
-client.once('ready', () => {
-    console.log('Tutorial Bot is online!');
-});
-
-client.login("SuperSecretToken");
